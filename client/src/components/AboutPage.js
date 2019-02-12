0fe476487bd1e89315f9e2fbceb886b40da1b539
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-02-12 15:38:02 -0700

    about us page started

---
+++ client/src/components/AboutPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import NavBar from "./NavBar"
+
+const AboutPage = () => {
+    return (
+
+        <section className="about">
+
+            <div className="u-center-text u-margin-bottom-big">
+                <h2 className="heading-secondary">
+                    <div className="box">
+                        About SkiLocal
+                    </div>
+
+
+                </h2>
+            </div>
+
+        </section>
+
+
+
+
+    )
+}
+
+export default AboutPage
