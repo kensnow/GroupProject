29784655f637d3f004ce9664fe720a2e95d3b7bd
Ken Snow <k.snowjr@gmail.com>
2019-02-12 16:28:15 -0700

    schema update

--- models/guide.js
+++ models/guide.js
@@ -11,20 +11,20 @@ const guideSchema = new mongoose.Schema({
         lowercase: true,
         required: true
     },
-    username: {
+    nickName: {
         type: String,
-        required: true,
-        unique: true,
         trim: true,
     },
+    firstName: String,
+    lastName: String,
     phoneNumber: String,
     password: {
         type: String,
         required: true
     },
-    userType:{
-        type:String,
-        enum:["Customer","Guide"]
+    userType: {
+        type: String,
+        enum: ["Customer", "Guide"]
     },
     profileImg: {
         type: Buffer,
@@ -32,8 +32,8 @@ const guideSchema = new mongoose.Schema({
     },
     aboutMe: String,
     bookings: [{
-        type:objectId,
-        ref:'Bookings'
+        type: objectId,
+        ref: 'Bookings'
     }],
     abilityRange: [],
     resorts: [{
