7a1d7c6b439a45223383edd66385f09555cac0e8
Ken Snow <k.snowjr@gmail.com>
2019-02-13 11:47:14 -0700

    added routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,14 +5,6 @@ const Header = () => {
     return (
         <header className="header">
             <NavBar />
-            <div className="text-box"> {/* add text */}
-                <h1 className="heading-primary">
-                    <span className="heading-primary-main">Ski with a local.</span> <br />
-                    <span className="heading-primary-sub">Ski like a local.</span>
-                </h1>
-                <a href="#" className="btn btn-white btn-animated"> Book Now </a>
-            </div>
-
         </header>
     )
 }
