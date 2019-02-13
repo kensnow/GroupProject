1bf89e8e5b0d9ec9be38115127b5db27cfb451d8
Jason Brown <jason.brown91@outlook.com>
2019-02-13 12:09:45 -0700

    A

--- models/guide.js
+++ models/guide.js
@@ -24,7 +24,7 @@ const guideSchema = new mongoose.Schema({
     },
     userType: {
         type: String,
-        enum: ["Customer", "Guide"]
+        enum: ["customer", "guide"]
     },
     profileImg: {
         type: Buffer,
