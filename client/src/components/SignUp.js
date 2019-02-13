3d185ae85e5c9f0afa9dbf9611b0d677be51fd1e
Jason Brown <jason.brown91@outlook.com>
2019-02-13 14:14:51 -0700

    Update Master

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,9 +3,9 @@ import React from 'react'
 const SignUp = () => {
     return (
         <div className="signupWrapper">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="text" placeholder="E-Mail" />
+            <input type="text" placeholder="First Name" name="fName" />
+            <input type="text" placeholder="Last Name" name="lName" />
+            <input type="text" placeholder="E-Mail" name="email" />
             <input type="password" placeholder="Password" />
             <input type="password" placeholder="E-Mail" />
             <button>Submit</button>
