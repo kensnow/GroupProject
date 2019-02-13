bf1dae92286a04d1af37ccfa846c8d5a3544332d
Jason Brown <jason.brown91@outlook.com>
2019-02-13 12:48:43 -0700

    Authentication

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="signupWrapper">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="E-Mail" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="E-Mail" />
+
             
         </div>
     )
