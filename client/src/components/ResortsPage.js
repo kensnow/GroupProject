4f503742ccde30e3c7478277fa4440ba1f0bbb0d
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-02-14 15:42:39 -0700

    resort cards are fucntional and styled and looking great might i add!

--- client/src/components/ResortsPage.js
+++ client/src/components/ResortsPage.js
@@ -17,14 +17,14 @@ function ResortsPage() {
                         <div className="card">
                             <div className="card__side card__side--front">
                                 <div className="card__picture card__picture--4"></div>
-                                <h4 className="card__heading">
-                                    <span className="card__heading--span card__heading--span--1" >SnowBird</span>
+                                <h4 className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >SnowBird</span>
                                 </h4>
                                 <div className="card__details">
                                     <ul>
-                                        <li>Terrain Ability</li>
-                                        <li>Skiable terrain</li>
-                                        <li>price</li>
+                                        <li>Intermediate to Beyond Expert</li>
+                                        <li>2,500 skiable acres</li>
+                                        <li>$$</li>
 
                                     </ul>
                                 </div>
@@ -32,9 +32,14 @@ function ResortsPage() {
 
                             <div className="card__side card__side--back card__side--back--1">
                                 <div className="card-cta">
-                                    <div className="card__price-box">
-                                        <p className="card__price-only">Only</p>
-                                        <p className="card__price-value">$297</p>
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info">
+
+                                            Snowbird ski resort is nestled in Little Cottonwood Canyon in the heart of the Wasatch-Cache National Forrest, approximately 29 miles from Salt Lake City International Airport.
+    
+    
+                                        </p>
+
                                     </div>
                                     <a href="#" className="btn btn-white">Book now!</a>
                                 </div>
@@ -46,14 +51,14 @@ function ResortsPage() {
                         <div className="card">
                             <div className="card__side card__side--front">
                                 <div className="card__picture card__picture--5"></div>
-                                <h4 className="card__heading">
-                                    <span className="card__heading--span card__heading--span--1" >Park City</span>
+                                <h4 className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >Park City</span>
                                 </h4>
                                 <div className="card__details">
                                     <ul>
-                                        <li>Intermediate to Pro</li>
-                                        <li>Park City Mountain Resort</li>
-                                        <li>9 years Skiing experience</li>
+                                        <li>Beginner to Expert</li>
+                                        <li> 7,300 skiable acres</li>
+                                        <li>$$$</li>
 
                                     </ul>
                                 </div>
@@ -61,9 +66,9 @@ function ResortsPage() {
 
                             <div className="card__side card__side--back card__side--back--1">
                                 <div className="card-cta">
-                                    <div className="card__price-box">
-                                        <p className="card__price-only">Only</p>
-                                        <p className="card__price-value">$197</p>
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info">Park City Mountain Resort is conveniently located within a 35-minute drive of Salt Lake City International Airport. With base and summit elevations at 6,800 feet and 10,000 feet, this massive mountain boasts 348 trails. </p>
+
                                     </div>
                                     <a href="#" className="btn btn-white">Book now!</a>
                                 </div>
@@ -76,23 +81,118 @@ function ResortsPage() {
                         <div className="card">
                             <div className="card__side card__side--front">
                                 <div className="card__picture card__picture--6"></div>
-                                <div className="card__heading">
-                                    <span className="card__heading--span card__heading--span--1" >Powder Mountain</span>
+                                <div className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >Powder Mountain</span>
                                 </div>
                                 <div className="card__details">
                                     <ul>
-                                        <li>Advanced to Pro ONLY</li>
-                                        <li>Powder Mountain, Snowbasin Resort</li>
-                                        <li>13 years Skiing experience</li>
+                                        <li>Beginner to Expert</li>
+                                        <li>8,464 Skiable acres</li>
+                                        <li>$</li>
 
                                     </ul>
                                 </div>
                             </div>
                             <div className="card__side card__side--back card__side--back--1">
                                 <div className="card-cta">
-                                    <div className="card__price-box">
-                                        <p className="card__price-only">Only</p>
-                                        <p className="card__price-value">$Billion</p>
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info"> Powder Mountain resort boasts the most skiable acreage of any resort in the United States. Powder Mountain offers a variety of terrain just east of Eden, Utah, just 55 miles from the Salt Lake City International Airport.</p>
+                                    </div>
+                                    <a href="#" className="btn btn-white">Book now!</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="cards-wrapper">
+                    <div className="col-1-of-3">
+                        <div className="card">
+                            <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--7"></div>
+                                <h4 className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >SnowBasin</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>Intermediate to Beyond Expert</li>
+                                        <li>2,500 skiable acres</li>
+                                        <li>$$</li>
+
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="card__side card__side--back card__side--back--1">
+                                <div className="card-cta">
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info">
+
+                                            Snowbird ski resort is nestled in Little Cottonwood Canyon in the heart of the Wasatch-Cache National Forrest, approximately 29 miles from Salt Lake City International Airport.
+    
+    
+                                        </p>
+
+                                    </div>
+                                    <a href="#" className="btn btn-white">Book now!</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-1-of-3">
+                        <div className="card">
+                            <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--8"></div>
+                                <h4 className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >Brighton</span>
+                                </h4>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>Beginner to Expert</li>
+                                        <li> 1,050 skiable acres</li>
+                                        <li>$</li>
+
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="card__side card__side--back card__side--back--1">
+                                <div className="card-cta">
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info">Brighton Resort is a great choice when looking for consistent snowfall combined with immaculate groomed trails and a wide variety of terrain. Brightons' terrain allows for beginners and intermediates to ski off of the same lift. </p>
+
+                                    </div>
+                                    <a href="#" className="btn btn-white">Book now!</a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="col-1-of-3">
+                        <div className="card">
+                            <div className="card__side card__side--front">
+                                <div className="card__picture card__picture--9"></div>
+                                <div className="card__headingResorts">
+                                    <span className="card__headingResorts--span card__headingResorts--span--1" >DeerValley</span>
+                                </div>
+                                <div className="card__details">
+                                    <ul>
+                                        <li>Beginner to Expert</li>
+                                        <li>2,000 Skiable acres</li>
+                                        <li>$$$$</li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="card__side card__side--back card__side--back--1">
+                                <div className="card-cta">
+                                    <div className="card__resort-info">
+                                        <p className="card__resort-info"> Deer Valley Resort is located just outside of Park City, Utah and is only 37 miles from Salt Lake International Airport. The ski resort is situated along the Wasatch Range and is home to great powder, groomers, steeps and moguls.</p>
                                     </div>
                                     <a href="#" className="btn btn-white">Book now!</a>
                                 </div>
