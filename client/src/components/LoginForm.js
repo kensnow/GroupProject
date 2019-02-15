e79764c99bf6dc103c83a9726a8dd9dbc99dbca1
Ken Snow <k.snowjr@gmail.com>
2019-02-15 14:08:12 -0700

    login nav complete

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import FormHandler from "../FormHandler"
 import {withDataHandler} from "../DataHandler"
+import {withRouter} from 'react-router-dom'
 
 const LoginForm = (props) => {
-    const inputs = {
+    const loginInputs = {
         email:"",
         password:""
 
     }
     return (
-        <FormHandler>
-            <form className="login-box">
-                <input type="email" name="email" />
-                <input type="password" name="password" />
+        <FormHandler inputs={loginInputs} submit={(inputs) => props.logIn(inputs).then(() => props.history.push('/myprofile'))}>
+            {
+                ({inputs, handleChange, handleSubmit}) => {
 
-                <button className="btn-white btn btn-animated"> Submit </button>
-            </form>
+                    return(
+                        <form onSubmit={handleSubmit} className="login-box">
+                            <input onChange={handleChange} type="email" name="email" />
+                            <input onChange={handleChange} type="password" name="password" />
+
+                            <button className="btn-white btn btn-animated"> Submit </button>
+                        </form>
+                    )
+                }
+            }
         </FormHandler>
-
     )
 }
 
-export default withDataHandler(LoginForm)
+export default withRouter(withDataHandler(LoginForm))
