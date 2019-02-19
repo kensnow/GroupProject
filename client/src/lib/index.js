34fc497d2e57c9e2cb3117fceca043c8829dbc0c
Ken Snow <k.snowjr@gmail.com>
2019-02-19 13:43:37 -0700

    added resort collection json object

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export default (() => {
 
     const getObjectData = (id, collectionArray) => {
-        
+        const foundElement = collectionArray.find(item => item._id === id )
+        return foundElement
     }
 
     //enter in helper functions here
