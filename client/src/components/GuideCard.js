9c5de1fedaa675533111eaf49be630fda9d89286
Jason Brown <jason.brown91@outlook.com>
2019-02-20 15:30:47 -0700

    Guide Card Image

--- client/src/components/GuideCard.js
+++ client/src/components/GuideCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link} from "react-router-dom"
 
 function GuideCard(props) {
-    const {_id, nickName, firstName, lastName, profileImg, dailyRate, aboutMe, bookings, abilityRange, resorts, reviews} = props
+    const {_id, nickName, firstName, lastName, avatar, profileImg, dailyRate, aboutMe, bookings, abilityRange, resorts, reviews} = props
 
     const resortMap = resorts.map((resort, i) =>{
         //need to incl;ude logic to search for resort id in state & get sub-info ie. name
@@ -29,6 +29,7 @@ function GuideCard(props) {
 
                 <div className="card__side card__side--back card__side--back--1">
                     <div className="card-cta">
+                    <img style={{width: "150px"}} src={`http://localhost:8080/image/${avatar}`} />
                         <div className="card__price-box">
                             <p className="card__price-only">Only</p>
                             <p className="card__price-value">${dailyRate}</p>
