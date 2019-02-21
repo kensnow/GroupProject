e60908e94ca09d9a4a75ee49c27b337983987473
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-02-21 14:06:13 -0700

    re wrote all of resorts to be uniqe from guides, rendering properly on resorts

--- client/src/components/ResortCard.js
+++ client/src/components/ResortCard.js
@@ -1,41 +1,43 @@
 import React from 'react'
 
 function ResortCard(props) {
-  
-    const {_id, name, city, state, guides, trails, description, priceRating, acres} = props
+
+    const { _id, name, city, state, guides, trails, description, priceRating, acres } = props
     //map through guides and create a guide cardlett for each
     return (
-        <div className="col-1-of-3">
-            <div className="card">
-                <div className="card__side card__side--front">
-                    <div className="card__picture card__picture--1"></div>
-                    <h4 className="card__heading">
-                        <span className="card__heading--span card__heading--span--1" >{name}</span>
-                    </h4>
-                    <div className="card__details">
-                        <ul>
-                            <li>Intermediate to Beyond Expert</li>
-                            <li>{acres.toLocaleString()} skiable acres</li>
-                            <li>{priceRating}</li>
+        
+            <div className="col-1-of-3">
+                <div className="cardResort">
+                    <div className="cardResort__sideResort cardResort__sideResort--frontResort">
+                        <div className="cardResort__pictureResort cardResort__pictureResort--4"></div>
+                        <h4 className="cardResort__headingResort">
+                            <span className="cardResort__headingResort--span cardResort__headingResort--spanResort--1Resort" >{name}</span>
+                        </h4>
+                        <div className="cardResort__detailsResort">
+                            <ul>
+                                <li>Intermediate to Beyond Expert</li>
+                                <li>{acres.toLocaleString()} skiable acres</li>
+                                <li>{priceRating}</li>
 
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="card__side card__side--back card__side--back--1">
-                    <div className="card-cta">
-                        <div className="card__info">
-                            <p className="card__info">
-                                {description}
-                            </p>
-
+                            </ul>
                         </div>
-                        <button className="resort-btn" onClick={() => props.bookService("resort", _id)} >Select {name}!</button>
+                    </div>
+
+                    <div className="cardResort__sideResort cardResort__sideResort--backResort cardResort__sideResort--backResort--1Resort">
+                        <div className="cardResort-ctaResort">
+                            <div className="cardResort__info">
+                                <p className="cardResort__info">
+                                    {description}
+                                </p>
+
+                            </div>
+                            <button className="resort-btn" onClick={() => props.bookService("resort", _id)} >Select {name}!</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+        
     )
 }
 
--- client/src/components/ResortsPage.js
+++ client/src/components/ResortsPage.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {withDataHandler} from '../DataHandler'
+import { withDataHandler } from '../DataHandler'
 import ResortCard from './ResortCard'
 
 function ResortsPage(props) {
 
-    const resortCollection = props.resorts.map((resort, i) => { return <ResortCard key={i} {...resort} bookService={props.bookService}/>})
+    const resortCollection = props.resorts.map((resort, i) => { return <ResortCard key={i} {...resort} bookService={props.bookService} /> })
 
     return (
 
-        <div className="page-img-resort"> 
+        <div className="page-img-resort">
             <div className="text-box-resort"> {/* add resort info */}
                 <h1 className="heading-primary-resort">
                     <span className="heading-primary-main-resort">Learn more about </span> <br />
@@ -16,11 +16,15 @@ function ResortsPage(props) {
                 </h1>
             </div>
             {/* cards start here */}
-            <div className="row"> 
-                <div className="cards-wrapper">
+            
+            <div className="row">
+                <div className="cards-wrapper-resort">
                     {resortCollection}
                 </div>
+
             </div>
+
+
 
         </div>
 
@@ -70,8 +74,8 @@ export default withDataHandler(ResortsPage)
 //                                         <p className="card__resort-info">
 
 //                                             Snowbird ski resort is nestled in Little Cottonwood Canyon in the heart of the Wasatch-Cache National Forrest, approximately 29 miles from Salt Lake City International Airport.
-    
-    
+
+
 //                                         </p>
 
 //                                     </div>
